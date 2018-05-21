bdfddc317a2c6a989283d45a34a24873a1d0154e
Baptiste <baptiste.meynier@gmail.com>
2018-05-21 23:22:58 +0200

    include tests

--- src/main/java/org/javaee7/test/wildfly/maven/plugin/jaxrs/MyResource.java
+++ src/main/java/org/javaee7/test/wildfly/maven/plugin/jaxrs/MyResource.java
@@ -1,14 +1,19 @@
 package org.javaee7.test.wildfly.maven.plugin.jaxrs;
 
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 
 @Path("/fruit")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class MyResource {
 
     @GET
---
+++ src/test/java/org/javaee7/test/wildfly/maven/plugin/jaxrs/FruitsClient.java
@@ -0,0 +1,31 @@
+package org.javaee7.test.wildfly.maven.plugin.jaxrs;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FruitsClient {
+
+	private String base="http://127.0.0.1:9990/WildflyMavenPlugin-0.0.1-SNAPSHOT.jar/webresources/fruit";
+	private static WebTarget target;
+
+	@Test
+	public void shouldContactWebservice() throws MalformedURLException {
+		final String aFruit= "Banana";
+		Client client = ClientBuilder.newClient();
+		target = client.target(URI.create(new URL(base).toExternalForm()));
+		target.request().post(Entity.json(aFruit));
+		Response response = target.request().get();
+		Assert.assertTrue(response.readEntity(String.class).contains(aFruit));
+	}
+
+}
